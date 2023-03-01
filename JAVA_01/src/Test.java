f3d2b422b6ed2a4f1fa929f73e1e32184f85a4a9
dancingcarrot <loopy456@naver.com>
2023-03-01 23:25:27 +0900

    My first eclipse commit!

--- JAVA_01/src/Test.java
+++ JAVA_01/src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("자바 처음 시작합니다.");
+		System.out.println("my first java .");
 	}
 
 }
