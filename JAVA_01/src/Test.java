c24d5f1c8e4e758537ea27317816ba117e70724c
dancingcarrot <loopy456@naver.com>
2023-03-02 18:43:55 +0900

    My first java commit!

--- JAVA_01/src/Test.java
+++ JAVA_01/src/Test.java
@@ -2,15 +2,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		int a =10;
+	
 		
-		System.out.print("나는 줄바꿈이 .");
-		System.out.print("안돼.");
-		System.out.println(" ");
-		System.out.println(" ");
-		System.out.println("나는 줄바꿈이 ");
-		System.out.println("자동으로 돼 .");
-		System.out.printf("%d",a);
+		System.out.println("My first java !.");
+		
 	}
 
 }
