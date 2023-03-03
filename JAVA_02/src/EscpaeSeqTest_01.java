44fb0aad9ebde7bdac977c0ce7066975a9869b89
dancingcarrot <loopy456@naver.com>
2023-03-03 21:47:19 +0900

    JAVA 제어문자 테스트/입출력 테스트/정렬 및 소수점 테스트/문자열 연결 테스트

--- JAVA_02/src/EscpaeSeqTest_01.java
+++ JAVA_02/src/EscpaeSeqTest_01.java
@@ -5,7 +5,7 @@ public class EscpaeSeqTest_01 {
 		System.out.println();
 		System.out.println("\t자바 프로그램"+"은"+"재밌다.");
 		System.out.println("\t\"java programming\'s very easy\"");
-		System.out.println("\n 자바 연결기호인 \"+\" " + "연산자 테스트");
+		System.out.println("\n자바 연결기호인 \"+\" " + "연산자 테스트");
 		System.out.println("내 영어 목표 점수는 " + "\\"+ "90점"+"\\"+"입니다.");
 	}
 
--- JAVA_02/src/StringConcatTest_01.java
+++ JAVA_02/src/StringConcatTest_01.java
@@ -3,7 +3,7 @@ public class StringConcatTest_01 {
 	public static void main(String[] args) {
 		int eng=100;  //변수
 		System.out.println("자바" + "프로그래밍");
-		System.out.println("국어: "+ "95점");
+		System.out.println("국어: " + "95점");
 		System.out.println("영어=" +eng+"점");
 		System.out.println(eng+"점");
 	}
